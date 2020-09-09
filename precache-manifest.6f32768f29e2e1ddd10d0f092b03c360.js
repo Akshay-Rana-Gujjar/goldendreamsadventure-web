33a74262b1ee58467f4b29e7bf971dabb0cdfaa7
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2020-09-09 20:15:24 +0530

    update

---
+++ precache-manifest.6f32768f29e2e1ddd10d0f092b03c360.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a7d3591ac4d98893612d67e621db87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "284f78104951360967eb",
+    "url": "/static/css/main.98a3bded.chunk.css"
+  },
+  {
+    "revision": "0e80e9405fce6c8aa8fe",
+    "url": "/static/js/2.88794533.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88794533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284f78104951360967eb",
+    "url": "/static/js/main.c674fe10.chunk.js"
+  },
+  {
+    "revision": "d553892f8f63912ee749",
+    "url": "/static/js/runtime-main.9f1e8064.js"
+  },
+  {
+    "revision": "f754aa4c833fa95e949139ad0c5b2df8",
+    "url": "/static/media/1.f754aa4c.jpg"
+  },
+  {
+    "revision": "0990abd20a64e4093c22f82c18561ce0",
+    "url": "/static/media/2.0990abd2.jpg"
+  },
+  {
+    "revision": "303a3a04c8c26167e3281cabac964b7f",
+    "url": "/static/media/3.303a3a04.jpg"
+  },
+  {
+    "revision": "32964f6e1b8b3e8ebdaaf971852ca4da",
+    "url": "/static/media/4.32964f6e.jpg"
+  },
+  {
+    "revision": "4063f11c1bbcdc8e4f94dcb2581d1ba3",
+    "url": "/static/media/ab-inner.4063f11c.jpg"
+  },
+  {
+    "revision": "eb22b7be4f9b58188c57af0ce985f1cd",
+    "url": "/static/media/about1.eb22b7be.jpg"
+  },
+  {
+    "revision": "d5c5da490fd08e7ec1b8d7b4fe03ea79",
+    "url": "/static/media/about2.d5c5da49.jpg"
+  },
+  {
+    "revision": "66be539980387c538556d988b99f8fb5",
+    "url": "/static/media/about3.66be5399.jpg"
+  },
+  {
+    "revision": "f540bdebf132de0765270eda13e1dc47",
+    "url": "/static/media/ba1.f540bdeb.jpg"
+  },
+  {
+    "revision": "9c20e8d1cd2bf8dca5b9887735fb8cf6",
+    "url": "/static/media/ba2.9c20e8d1.jpg"
+  },
+  {
+    "revision": "8f0dcdbda2aa6300451625bc2840a7d1",
+    "url": "/static/media/bangkok.8f0dcdbd.webp"
+  },
+  {
+    "revision": "ae53021ebd0c4e70ce879358ca173d78",
+    "url": "/static/media/bg1.ae53021e.jpg"
+  },
+  {
+    "revision": "f5bd459ba1c7568c9607bbeb913f2cfe",
+    "url": "/static/media/chiang-mai.f5bd459b.webp"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont3e6e.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont3e6e.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont3e6e.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont3e6e.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont3e6e.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfontd41d.674f50d2.eot"
+  },
+  {
+    "revision": "23a5ebf30f8753f3af0f69998a55224f",
+    "url": "/static/media/goldendreamsadventure-logo-footer.23a5ebf3.png"
+  },
+  {
+    "revision": "b5da7c868454947fe965b65ae444fe9a",
+    "url": "/static/media/inner.b5da7c86.jpg"
+  },
+  {
+    "revision": "410887fea6fc12fb1b74380f6fd1acba",
+    "url": "/static/media/inner2.410887fe.jpg"
+  },
+  {
+    "revision": "aeec22af6bd81ff6a3e16e6f846d2a05",
+    "url": "/static/media/ko-samui.aeec22af.webp"
+  },
+  {
+    "revision": "b2e0380662f8f97dc7fc271aabed6b72",
+    "url": "/static/media/package1.b2e03806.jpg"
+  },
+  {
+    "revision": "11aee16462a1060f4e63d9ed47a13b44",
+    "url": "/static/media/package2.11aee164.jpg"
+  },
+  {
+    "revision": "4a39b707adc0d135e6fc7f199af84265",
+    "url": "/static/media/package3.4a39b707.jpg"
+  },
+  {
+    "revision": "a2045ddb7ece3dc8d05e91663d9ca910",
+    "url": "/static/media/package4.a2045ddb.jpg"
+  },
+  {
+    "revision": "d0ccdbb46febf3853212a0af8f872f37",
+    "url": "/static/media/package5.d0ccdbb4.jpg"
+  },
+  {
+    "revision": "74cedadf89573442f0d6a60cf7c7622a",
+    "url": "/static/media/pattaya-walking-street.74cedadf.webp"
+  },
+  {
+    "revision": "b84bc3633937b54bdc14a572a5cb358c",
+    "url": "/static/media/phi-phi-island.b84bc363.webp"
+  },
+  {
+    "revision": "e890b0117f22749a1c36fbdd0d980830",
+    "url": "/static/media/phuket.e890b011.webp"
+  }
+]);
